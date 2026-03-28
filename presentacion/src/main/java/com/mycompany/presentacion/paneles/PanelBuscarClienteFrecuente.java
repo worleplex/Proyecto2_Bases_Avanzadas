93157bff66eea477836a136b05481f19f4dd64fa
Luis Alonso <luisalonsoortiz343@gmail.com>
2026-03-28 00:03:56 -0700

    Panel para buscar clientes frecuentes

---
+++ presentacion/src/main/java/com/mycompany/presentacion/paneles/PanelBuscarClienteFrecuente.java
@@ -0,0 +1,40 @@
+package com.mycompany.presentacion.paneles;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PanelBuscarClienteFrecuente extends JPanel {
+    private Image imagen;
+
+    public PanelBuscarClienteFrecuente() {
+        imagen = new ImageIcon("presentacion\\src\\main\\java\\com\\mycompany\\presentacion\\fondos\\FondoInicio.png").getImage();
+
+    }
+
+
+    public void mostrar() {
+        setSize(1080, 720);
+        setLayout(new BorderLayout(20, 20));
+
+        JPanel panelFondo = new JPanel(new BorderLayout()) {
+
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+
+                Graphics2D g2 = (Graphics2D) g;
+
+                // Habilitar alta calidad
+                g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+                g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+                g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+                // Dibujar imagen con interpolación suave
+                g2.drawImage(imagen, 0, 0, getWidth(), getHeight(), null);
+            }
+        };
+
+
+        setVisible(true);
+    }
+}
