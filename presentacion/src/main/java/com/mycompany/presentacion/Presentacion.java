deb79cde936f5b124acfba4758a88cbe040fbbb4
Luis Alonso <luisalonsoortiz343@gmail.com>
2026-03-27 23:30:17 -0700

    Pantalla de inicio de sesion

--- presentacion/src/main/java/com/mycompany/presentacion/Presentacion.java
+++ presentacion/src/main/java/com/mycompany/presentacion/Presentacion.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.presentacion;
 
+import com.mycompany.presentacion.paneles.PanelLogin;
+
 /**
  *
  * @author Gael Galaviz
@@ -11,6 +13,7 @@ package com.mycompany.presentacion;
 public class Presentacion {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        PanelLogin panelLogin = new PanelLogin();
+        panelLogin.mostrar();
     }
 }
