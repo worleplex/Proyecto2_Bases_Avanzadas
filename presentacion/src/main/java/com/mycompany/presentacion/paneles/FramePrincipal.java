6f3b38e255ceeac770c64c626a4cdeafb360063c
Luis Alonso <luisalonsoortiz343@gmail.com>
2026-04-01 12:49:46 -0700

    Agregue este frame ahora como frame principal que manejara todas las pantallas pero hay un detalle sobre como muestra la pantalla voy a seguir checandolo

---
+++ presentacion/src/main/java/com/mycompany/presentacion/paneles/FramePrincipal.java
@@ -0,0 +1,41 @@
+package com.mycompany.presentacion.paneles;
+
+import com.mycompany.presentacion.controlador.Coordinador;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class FramePrincipal extends JFrame {
+    public final Coordinador coordinador;
+    private Image imagen;
+
+    public void cambiarPanel(JPanel panel){
+        getContentPane().removeAll();
+        getContentPane().add(panel, BorderLayout.CENTER);
+        revalidate();
+        repaint();
+        panel.requestFocusInWindow();
+    }
+
+    public FramePrincipal(Coordinador coordinador) {
+        this.coordinador = coordinador;
+        java.net.URL url = getClass().getResource("/FondoInicio.png");
+        if (url != null) {
+            this.imagen = new ImageIcon(url).getImage();
+        } else {
+            System.err.println("Error: No se encontro FondoInicio.png");
+        }
+        mostrar();
+        setVisible(true);
+    }
+
+
+    public void mostrar(){
+        setSize(1080,720);
+        setTitle("Pantalla de Eleccion");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        PanelElegir panelElegir = new PanelElegir(coordinador);
+        add(panelElegir, BorderLayout.CENTER);
+    }
+}
