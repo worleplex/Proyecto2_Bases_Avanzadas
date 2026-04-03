8779c611a63bba68272a0d5888339d3f051444d4
Luis Alonso <luisalonsoortiz343@gmail.com>
2026-04-02 19:43:11 -0700

    Agregue algunos metodos auxiliares para la navegacion entre paneles

--- presentacion/src/main/java/com/mycompany/presentacion/controlador/Coordinador.java
+++ presentacion/src/main/java/com/mycompany/presentacion/controlador/Coordinador.java
@@ -1,25 +1,58 @@
 package com.mycompany.presentacion.controlador;
 
-import com.mycompany.presentacion.paneles.FramePrincipal;
-import com.mycompany.presentacion.paneles.PanelElegir;
-import com.mycompany.presentacion.paneles.PanelLogin;
-import com.mycompany.presentacion.paneles.PanelMenuClientes;
+import com.mycompany.presentacion.paneles.*;
+
+import javax.swing.*;
 
 /**
  *  @author Luis
  **/
 public class Coordinador {
     FramePrincipal framePrincipal;
+    PanelLogin panelLogin;
+    PanelElegir panelElegir;
+    PanelMenuMesero panelMenuMesero;
+    PanelMenuAdmin panelMenuAdmin;
 
     public Coordinador(){
 
     }
-
     public void iniciarSistema() {
         if (framePrincipal == null) {
             framePrincipal = new FramePrincipal(this);
         }
         framePrincipal.setVisible(true);
+    }
+
+    public void cambiarTituloFrame(String nombre){
+        framePrincipal.setTitle("Bienvenido al sistema " + nombre);
+    }
+
+
+    public void cambiarPanel(JPanel panel) {
+        JFrame v = (JFrame) SwingUtilities.getWindowAncestor(framePrincipal);
+        v.setContentPane(panel);
+        v.revalidate();
+    }
+
+    public void mostrarPanelLogin(){
+        if (panelLogin == null) {
+            panelLogin = new PanelLogin(this);
+        }
+        cambiarPanel(panelLogin);
+    }
+
+    public void mostrarPanelElegir(){
+        if(panelElegir == null){
+            panelElegir = new PanelElegir(this);
+        }
+        cambiarPanel(panelElegir);
+    }
+
+    public void mostrarPanelMenuAdmin(){
+        if(panelMenuAdmin == null){
+            panelMenuAdmin = new PanelMenuAdmin(this);
+        }
     }
 
 }
